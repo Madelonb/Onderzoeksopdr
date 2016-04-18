4a88b70e97571ef2aa6812ce38876d59bfe024fa
Madelon Balk <madelon.balk@gmail.com>
2016-04-18 16:25:12 +0200

    simpel voorbeeld

---
+++ src/Ma_01.java
@@ -0,0 +1,25 @@
+/**
+ * Created by madelonbalk on 18-04-16.
+ */
+import processing.core.*;
+
+/**
+ * Created by madelonbalk on 31-03-16.
+ */
+public class Ma_01 extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("Ma_01", args);
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+
+    }
+
+
+
+}
